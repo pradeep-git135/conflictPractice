30ecf6c66ccd98837247dba721a6c0baea2c7454
NitheshHS <nitheshsgowda4.ng@gmail.com>
2022-01-08 13:03:48 +0530

    from selenium workspace

--- src/test/java/package1/Sample1.java
+++ src/test/java/package1/Sample1.java
@@ -5,7 +5,7 @@ public class Sample1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("first print statement");
-		System.out.println("from selenium-workspace");
+		System.out.println("  from selenium-workspace");
 
 	}
 
