1c215ea8b90e6e1c2d9ea4daa49333d566643bdf
NitheshHS <nitheshsgowda4.ng@gmail.com>
2022-01-08 13:01:38 +0530

    from selenium java

--- src/test/java/package1/Sample1.java
+++ src/test/java/package1/Sample1.java
@@ -5,7 +5,7 @@ public class Sample1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("first print statement");
-		System.out.println("second commit from selenium java workspace");
+		System.out.println("second commit from selenium java workspace ");
 	}
 
 }
